66dc7d9f0446956d47da7029b2abb7984147e970
wasimakram97091 <wasimakram97091@gmail.com>
2024-01-11 01:59:08 +0530

    autoplay added

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -20,7 +20,7 @@ function WatchPage() {
           <div className="xs:w-[280px]">
             <iframe
               className="xs:w-[280px] sm:w-[375px] sm:h-[250px] sml:w-[500px] sml:h-[300px] md:w-[667px] mdl:w-[768px] mdl:h-[500px] lg:w-[992px] xl:w-[1280px] xl:h-[600px]"
-              src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+              src={"https://www.youtube.com/embed/" + searchParams.get("v") + "?&autoplay=1"}
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
